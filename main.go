6fac52a10db313cb746d52685e46dc8a8d858def
jack <hhdproject>
2023-02-15 23:49:32 +0800

    debug

--- main.go
+++ main.go
@@ -1,4 +1,11 @@
-package mypro
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"time"
+)
 
 type Response struct {
 	Code int         `json:"code"`
@@ -7,5 +14,18 @@ type Response struct {
 }
 
 func Hello(ctx *gin.Context) {
-
+	resp := &Response{
+		Code: 0,
+		Msg:  "succ",
+		Data: fmt.Sprintf("hello world! %s,", time.Now()),
+	}
+	ctx.JSON(http.StatusOK, resp)
+}
+func main() {
+	router := gin.Default()
+	router.GET("/", Hello)
+	err := router.Run(":8888")
+	if err != nil {
+		fmt.Println("ddd")
+	}
 }
